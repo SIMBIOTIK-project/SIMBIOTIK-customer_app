f7c9deaea11aaa7145537dd11dc3fa43611b3f30
ASNProject <asnproject02@gmail.com>
2024-08-18 21:34:31 +0700

    update

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:simbiotik_customer/app.dart';
 import 'package:simbiotik_customer/core/configs/app_config.dart';
 import 'package:simbiotik_customer/core/configs/flavor_type.dart';
+import 'package:simbiotik_customer/firebase_options.dart';
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final devAppConfig = AppConfig(
     appName: 'SIMBIOTIK Access DEV',
     flavorType: FlavorType.dev,
